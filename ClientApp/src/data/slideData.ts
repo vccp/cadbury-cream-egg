99e14d8361a9e14e70f821036ca7350a8a89b679
legranddigital <thibault@legranddigital.com>
2024-01-09 13:37:15 +0100

    update backgrounds and text-shafow

--- ClientApp/src/data/slideData.ts
+++ ClientApp/src/data/slideData.ts
@@ -1,63 +1,63 @@
 export const slideData = [
     {
         id: 1,
-        backgroundImage: '../assets/images/BackgroundPurpleDesktop.webp',
+        backgroundImage: '../assets/images/Purple-Desktop.webp',
         imageUrl: '../assets/images/Egg-Landing.webp',
         backgroundColor: '#3B0073',
         color: '#FFDD00'
     },
     {
         id: 2,
-        backgroundImage: '../assets/images/BackgroundPurpleDesktop.webp',
+        backgroundImage: '../assets/images/Yellow-Desktop.webp',
         imageUrl: '../assets/images/Egg-Landing.webp',
         backgroundColor: '#3B0073',
-        color: '#FFDD00'
+        color: '#3B0073'
     },
     {
         id: 3,
-        backgroundImage: '../assets/images/BackgroundPurpleDesktop.webp',
+        backgroundImage: '../assets/images/Purple-Desktop.webp',
         backgroundColor: '#ffffff',
         color: '#ffffff'
     },
     {
         id: 4,
-        backgroundImage: '../assets/images/BackgroundRedDesktop.webp',
+        backgroundImage: '../assets/images/Red-Desktop.webp',
         backgroundColor: '#FFDD00',
         color: '#FFDD00'
     },
     {
         id: 5,
-        backgroundImage: '../assets/images/BackgroundWhiteDesktop.webp',
+        backgroundImage: '../assets/images/White-Desktop.webp',
         backgroundColor: 'red',
         color: '#3B0073'
     },
     {
         id: 6,
-        backgroundImage: '../assets/images/BackgroundYellowDesktop.webp',
+        backgroundImage: '../assets/images/Yellow-Desktop.webp',
         backgroundColor: 'white',
         color: '#3B0073'
     },
     {
         id: 7,
-        backgroundImage: '../assets/images/BackgroundRedDesktop.webp',
+        backgroundImage: '../assets/images/Red-Desktop.webp',
         backgroundColor: '#FFDD00',
         color: '#FFDD00'
     },
     {
         id: 8,
-        backgroundImage: '../assets/images/BackgroundPurpleDesktop.webp',
+        backgroundImage: '../assets/images/Purple-Desktop.webp',
         backgroundColor: '#FFDD00',
         color: 'white'
     },
     {
         id: 9,
-        backgroundImage: '../assets/images/BackgroundPurpleDesktop.webp',
+        backgroundImage: '../assets/images/Purple-Desktop.webp',
         backgroundColor: '#3B0073',
         color: '#FFDD00'
     },
     {
         id: 10,
-        backgroundImage: '../assets/images/BackgroundPurpleDesktop.webp',
+        backgroundImage: '../assets/images/Purple-Desktop.webp',
         backgroundColor: '#3B0073',
         color: '#FFDD00'
     }
