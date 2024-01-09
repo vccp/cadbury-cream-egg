3e4098a7a88be2b715264057a6ad36ce673cdcae
legranddigital <thibault@legranddigital.com>
2024-01-09 12:04:46 +0100

    copy update

--- ClientApp/src/components/QuizForm.tsx
+++ ClientApp/src/components/QuizForm.tsx
@@ -141,8 +141,8 @@ const QuizForm = () => {
                         <div ref={creamLogoRef}>
                             <img className="cadburyCreamLogoImage" src={CadburyCremeEggLogoVectorRGB} alt="" />
                         </div>
-                        <h1 ref={mainHeadingRef} className="main-heading text-yellow">WIN WIN WIN</h1>
-                        <h2 ref={secondaryHeadingRef} className="secondary-heading text-white">ENTER TO WIN</h2>
+                        <h1 ref={mainHeadingRef} className="main-heading text-yellow">WIN YOUR DREAM PRIZE!</h1>
+                        <h2 ref={secondaryHeadingRef} className="secondary-heading text-white">ENTER TO WIN THE PERFECT PRIZE<br/> FOR WHO YOU REALLY ARE</h2>
                         <div ref={formWrapperRef} className="quiz-form-wrapper">
                             <form onSubmit={handleSubmit}>
                                 <div className='formGroup'>
@@ -212,7 +212,7 @@ const QuizForm = () => {
                                             id='checkbox1'
                                             name='checkbox1'
                                         />
-                                        <label htmlFor='checkbox1'>T&Cs/privacy for competition (TBC on actual copy)</label>
+                                        <label htmlFor='checkbox1'>Tick for the chance to win a prize</label>
                                     </div>
                                     <h4 className="text-white keepUpToDate">KEEP UP TO DATE</h4>
                                     <div className='checkbox'>
